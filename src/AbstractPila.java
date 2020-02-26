49433e38922ba93e314f6ac3f5d4cb085543517e
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-02-25 18:02:22 -0600

    ya estan todas las clases

--- src/AbstractPila.java
+++ src/AbstractPila.java
@@ -8,11 +8,22 @@
  */
 public abstract class AbstractPila<E> implements Pila<E>{
 
+    /**
+     * Se encarga de agregar un valor al final de la lisa
+     * @pos se agrega un nuevo valor a la pila
+     * @param value valor que se quiere agregar
+     */
     @Override
     public void add(E value) {
         add(value);
     }
 
+    /**
+     * Se encarga de eliminar al primer elemento de la lista
+     * @pre la pila debe de tener al menos un valor
+     * @pos se le quita un valor a la pila
+     * @return el valor eliminado
+     */
     @Override
     public E remove() {
         return remove();
